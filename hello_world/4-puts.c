4400df913a1cbdca8591d66f77806cd90dd3ce6a
Pancitoconcafe <alex115.jpg@gmail.com>
2022-10-09 11:40:06 -0700

    a

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main (void)
+int main(betty)
 {
-	        puts ("Programming is like building a multilingual puzzle\n");
+	        puts("Programming is like building a multilingual puzzle\n");
 		        return (0);
 }
