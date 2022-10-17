401e128f110dfd3f24c84a251a62976436db4b36
Pancitoconcafe <alex115.jpg@gmail.com>
2022-10-17 09:04:27 -0700

    all6

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *more_numbers - check the code.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int c = 0;
+	int b = 0;
+
+	do {
+		while (b <= 14)
+		{
+			if (b > 9)
+				_putchar(b / 10 + '0');												_putchar(b % 10 + '0');													b++;			
+		}
+		_putchar('\n');
+		c++;
+		b = 0;
+	} while (c <= 9);
+}
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_line - check the code.
+ *@n: variable to check
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		n--;
+		_putchar('_');
+	}
+	_putchar('\n');
+}
