309853a53088758ce7b2f9e39a59ac22cc7a8eb1
Pancitoconcafe <alex115.jpg@gmail.com>
2022-10-28 14:33:42 -0700

    4-5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *sqrt_square - Is that make the square rooot
+ *@number: Is the result of the square root
+ *@re: Is n
+ * Return: Always 0.
+ */
+int sqrt_square(int number, int re)
+{
+	if (number * number == re)
+	{
+		return (number);
+	}
+	else if (number * number > re)
+	{
+		return (-1);
+	}
+	return (sqrt_square(number + 1, re));
+}
+int _sqrt_recursion(int n)
+{
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (sqrt_square(1, n));
+}
