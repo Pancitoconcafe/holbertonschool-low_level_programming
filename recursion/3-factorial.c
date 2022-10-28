4c6184c04dd8b3158c0f27158a99e9e95e6f6e27
Pancitoconcafe <alex115.jpg@gmail.com>
2022-10-28 13:54:11 -0700

    3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+	if(n==0 || n=1)
+	{
+	else
+		n = (n - 1);
+	}
+	return n;
+}
