f7a1908ce9151f27a82d2339a958f2d0b7d57c6d
Pancitoconcafe <alex115.jpg@gmail.com>
2022-10-31 09:56:02 -0700

    taskt1

--- static_libraries/0-memset.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "main.h"
-
-char *_memset(char *s, char b, unsigned int n)
-{
-
-}
--- static_libraries/_putchar.c
+++ static_libraries/_putchar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
-int _putchar(int c)
+int _putchar(char c)
 {
 
 }
